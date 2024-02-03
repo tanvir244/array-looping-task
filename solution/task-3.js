2e11de14b79f8de6fdc69dca33266d9637d69b2c
tanvir243 <travelertanvir@gmail.com>
2024-02-03 16:40:53 +0600

    task-3 completed

---
+++ solution/task-3.js
@@ -0,0 +1,11 @@
+// task-3
+
+let numbers = ['Tom', 'Tim', 'Tin', 'Tik'];
+let concatination = [];
+
+for(let iterators of numbers){
+    concatination.push(iterators);
+}
+let result = concatination.join('');
+console.log(numbers);
+console.log(result);
